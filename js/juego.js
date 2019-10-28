140454876da364fb19baddd9cec0230c2e9f725a
albertosanchezmateo <35465627+albertosanchezmateo@users.noreply.github.com>
2019-10-28 12:31:38 +0100

    Revert "Update juego.js"
    This reverts commit 024d0b7fe1ffcb7c38c02f2822ca89b77be4aa07.

--- js/juego.js
+++ js/juego.js
@@ -4,7 +4,7 @@ var config = {
 	expandParent :true,
 	width: 800,
 	height: 600,
-    scene: [pantallaPressStart,menuPrincipal],
+    scene: [menuPrincipal],
 	scale: {
    
      autoCenter: Phaser.Scale.CENTER_BOTH,
