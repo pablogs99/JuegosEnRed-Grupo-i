b7c46a2be5b2122d603213d646924a0a614fb9c0
albertosanchezmateo <35465627+albertosanchezmateo@users.noreply.github.com>
2019-11-13 19:51:45 +0100

    Push Final Fase 2

--- js/juego.js
+++ js/juego.js
@@ -2,13 +2,19 @@ var config = {
     type: Phaser.AUTO,
     parent: "canvas",
 	expandParent :true,
-	width: 800,
+	width: 1000,
 	height: 600,
-    scene: [menuPrincipal],
+    scene: [pantallaPressStart,menuPrincipal,selectorPersonaje,seleccionMapa,combate],
 	scale: {
-   
      autoCenter: Phaser.Scale.CENTER_BOTH,
     },
+	physics:{
+		default :'arcade',
+		arcade : {
+			debug : false,
+			graviy: {y:10000},
+		},
+	}
 };
 
 var game = new Phaser.Game(config);
--- js/pantallaPressStart.js
+++ js/pantallaPressStart.js
@@ -1,30 +1,51 @@
 class pantallaPressStart extends Phaser.Scene {
-    constructor() {
-      super("pressStart"); 
-    }
+  constructor() {
+    super("pressStart");
+  }
 
-    preload(){
-        this.load.image("logo", "../assets/images/userInterface/logoPAWStudios.png");
-    }
-    create(){
-        this.add.image()
+  preload() {
+    this.load.image("logo", "../assets/images/userInterface/logoPAWStudios.png");
+  }
+  create() {
+    this.add.image()
 
-        let logo = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2 - 60, "logo").setScale(0.35);
-        this.add.text(this.game.renderer.width / 2 - 115 , this.game.renderer.height / 2 + 240, "Press any button to start");
+    let logo = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2 - 60, "logo").setScale(0.35);
+    let texto = this.add.text(this.game.renderer.width / 2 - 115, this.game.renderer.height / 2 + 240, "Press any button to start");
 
-        this.input.on('pointerup', function (pointer) {
+    logo.alpha = 0;
+    this.tweens.add({
+      targets: logo,
+      alpha: 1,
+      duration: 3750,
+      yoyo: false,
+      delay: 0
+    });
 
-            this.scene.start('menu');
+    texto.alpha = 0;
+    this.tweens.add({
+      targets: texto,
+      alpha: 1,
+      duration: 1500,
+      yoyo: true,
+      loop: -1, //infinito
+      delay: 500
 
-        }, this);
+    });
 
-        this.input.keyboard.on('keydown', function () {
+    this.input.on('pointerup', function(pointer) {
 
-         this.scene.start('menu');
+      this.scene.start('menu');
 
-        }, this);
+    }, this);
 
-    }
+    this.input.keyboard.on('keydown', function() {
 
+      this.scene.start('menu');
+
+    }, this);
+
+    this.game.config.suena = false;
+    console.log(this.game.config.suena)
+  }
 
 }
